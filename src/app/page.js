a065ce471c67ebae1ead29b09e9ebab391cdcf83
Akintayo74 <akinnibosunakintayo74@gmail.com>
2025-07-18 22:55:57 +0100

    File structure: added (auth) and api directories under app folder

--- src/app/page.js
+++ src/app/page.js
@@ -5,16 +5,16 @@ function Home() {
 
   return (
     <>
-      <div className="grid grid-cols-2 bg-primary min-h-full justify-items-center">
-        <div className="grid place-content-center">
+      <div className="grid grid-cols-2 bg-primary min-h-full place-items-center">
+        <div className="flex flex-col justify-center max-w-2xl ms-[31px] gap-[calc(31/16*1rem)]">
           <span className="flex">
             <Image src='/Logo.png' alt="Logo" width={50} height={50}/>
-            <h1 className="text-6xl text-primary-700">Sprinter</h1>
+            <h1 className="text-6xl text-primary-500 font-bold italic">Sprinter</h1>
           </span>
 
-          <span className="text-interface text-4xl">Run Your Projects Like Clockwork</span>
+          <span className="text-interface text-4xl font-bold">Run Your Projects Like Clockwork</span>
 
-          <p className="text-primary-900 max-w-sm">Organize tasks, track progress, and keep your team in sync all from one sleek collaborative workspace</p>
+          <p className="text-primary-900 max-w-xs font-bold leading-[1.4]">Organize tasks, track progress, and keep your team in sync all from one sleek collaborative workspace</p>
 
 
         </div>
